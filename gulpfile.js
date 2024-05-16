cff106bd35c4a47a9ee66074874893d337a7c821
Maciej Szostak <m.szostak@esoleo.pl>
2024-05-16 14:11:46 +0200

    dodaj kolejna ścieżkę

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,8 @@
 var gulp = require("gulp");
-var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
-var rename = require("gulp-rename");
 
 gulp.task("buildjs", function () {
-  return gulp
-    .src("*.js")
-    .pipe(concat("all.js"))
-    .pipe(gulp.dest("dist"))
-    .pipe(rename("all.min.js"))
+  return gulp.src("source/*.js")
     .pipe(uglify())
     .pipe(gulp.dest("dist"));
 });
